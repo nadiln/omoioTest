99f6e2181cd3b656a04d419e43cc8c3baeb4ddd0
Nadil <deempade@gmail.com>
2020-02-05 06:06:29 +0530

    SignIn component UI

--- src/app/screens/signIn/component.tsx
+++ src/app/screens/signIn/component.tsx
@@ -1,7 +1,8 @@
-import {Text, View} from 'react-native';
+import {Alert, Button, Text, View} from 'react-native';
 
 import Container from './container';
 import React from 'react';
+import { TextInput } from 'react-native-gesture-handler';
 import styles from './style.js';
 
 interface Props {}
@@ -10,6 +11,24 @@ const Login: React.FunctionComponent<Props> = ({}) => {
   return (
     <View style={styles.container}>
       <Text style={styles.loginText}>SignIn</Text>
+      <View style={styles.formInputContainer}>
+        <Text style={styles.inputLabel}>Email</Text>
+        <TextInput
+      style={styles.textInput}
+    />
+      </View>
+      <View style={styles.formInputContainer}>
+        <Text style={styles.inputLabel}>Password</Text>
+        <TextInput
+      style={styles.textInput}
+    />
+      </View>
+      <View style={styles.buttonContainer}>
+       <Button
+          title="Sign In"
+          onPress={ () => Alert.alert("SignIn")}
+/>
+          </View>
     </View>
   );
 };
--- src/app/screens/signIn/style.js
+++ src/app/screens/signIn/style.js
@@ -5,5 +5,42 @@ export default StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     flex: 1,
-  }
+    height: 300,
+    flexDirection: 'column',
+ },
+  loginText: {
+    fontSize: 14,
+    height: 100,
+  },
+  formInputContainer: {
+    flexDirection: 'row',
+    padding: 10,
+    height: 50,
+    // backgroundColor: 'red',
+    borderBottomColor: 'blue',
+    marginBottom: 10,
+
+  },
+  inputLabel: {
+    flex: 2,
+    width:60,
+    alignItems: 'flex-start',
+    height: 40,
+  },
+  textInput: {
+    width: 150,
+    height: 40,
+    // borderColor: 'gray',
+    borderWidth: 1,
+    flex: 5,
+    alignItems: 'flex-end'
+  },
+  buttonContainer: {
+    height: 50,
+    alignSelf: 'flex-end',
+    padding: 10,
+    // flex: 1,
+    // flexDirection: 'row',
+    // justifyContent: 'flex-end',
+    },
 });
